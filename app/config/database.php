274f9d6f4605f173c8db0b088122e32306768791
Rougin Gutib <rougingutib@gmail.com>
2016-01-16 14:33:38 +0800

    Moved configurations to config directory

---
+++ app/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'mysql' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'dbname' => 'demo',
+        'charset' => 'utf8',
+    ]
+];
