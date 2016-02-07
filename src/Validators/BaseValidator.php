ede6368a8ee56cf5d89d08e91585b7f668adf924
Rougin Royce Gutib <rougingutib@gmail.com>
2016-02-08 00:49:38 +0800

    Added BaseValidator.php

---
+++ src/Validators/BaseValidator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Validators;
+
+use Valitron\Validator;
+
+/**
+ * Base Validator
+ * 
+ * @package App
+ * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ */
+class BaseValidator implements ValidatorInterface
+{
+    /**
+     * @var array
+     */
+    protected $errors = [];
+
+    /**
+     * Returns a listing of error, if any.
+     * 
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Validates the data from the registration page.
+     * 
+     * @param  array  $data
+     * @return boolean
+     */
+    public function validate(array $data)
+    {
+        $validator = new Validator($data);
+
+        $validator->rule('required', $this->required);
+
+        if ( ! $validator->validate()) {
+            $this->errors = $validator->errors();
+
+            return false;
+        }
+
+        return true;
+    }
+}
