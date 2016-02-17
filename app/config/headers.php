c869b22b6e31a17897e778280a95cff4bf8f47f6
Rougin Royce Gutib <rougingutib@gmail.com>
2016-02-17 10:37:54 +0800

    Add $_SERVER headers in headers.php

--- app/config/headers.php
+++ app/config/headers.php
@@ -1,9 +1,20 @@
 <?php
 
+$allowedHeaders = [];
+$allowedOrigins = [];
+
+if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+    array_push($allowedHeaders, $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+}
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    array_push($allowedOrigins, $_SERVER['HTTP_ORIGIN']);
+}
+
 return [
     'support_credentials' => false,
-    'allowed_origins' => [$_SERVER['HTTP_ORIGIN']],
-    'allowed_headers' => [$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']],
+    'allowed_origins' => $allowedOrigins,
+    'allowed_headers' => $allowedHeaders,
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
     'exposed_headers' => [],
     'max_age' => 0,
