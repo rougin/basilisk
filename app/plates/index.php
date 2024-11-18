57348c1a163193c56a2b2508b8749410a27c938d
Rougin Gutib <rougingutib@gmail.com>
2024-11-18 21:18:31 +0800

    Use "RendererIntegration" as default

--- app/plates/index.php
+++ app/plates/index.php
@@ -4,15 +4,15 @@
   <meta charset="UTF-8">
   <title>Basilisk - A skeleton application for the Slytherin framework</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-
-  @yield('styles')
 </head>
 <body>
-  @yield('content')
+  <div class="container">
+    <div class="p-5">
+      <h1 class="text-center">Hello, Muggle!</h1>
+    </div>
+  </div>
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
-
-  @yield('scripts')
 </body>
 </html>
